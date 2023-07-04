d1ba65cea26a9682abb1092b64bf711382bcb443
alek <wait82985@gmail.com>
2023-07-04 22:59:01 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,26 @@
 public class Solution {
     public int minimumTimeRequired(int[] jobs, int k) {
-        //TODO
-        return 0;
+
+        int[] workers = new int[k];
+        return dfs(jobs, workers, 0);
+    }
+
+    private int dfs(int[] jobs, int[] workers, int i) {
+        if (i == jobs.length) {
+            int max = Integer.MIN_VALUE;
+            for (int worker : workers) {
+                max = Math.max(worker, max);
+            }
+            return max;
+        }
+        int result = Integer.MAX_VALUE;
+        for (int j = 0; j < workers.length; j++) {
+            workers[j] += jobs[i];
+            result = Math.min(result, dfs(jobs, workers, i + 1));
+            workers[j] -= jobs[i];
+            if(workers[j] == 0) break;
+
+        }
+        return result;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -21,4 +21,14 @@ public class SolutionTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    /**/
+    @Test
+    public void minimumTimeRequiredTest3() {
+        int[] jobs = {9899456,8291115,9477657,9288480,5146275,7697968,8573153,3582365,3758448,9881935,2420271,4542202};
+        int k = 9;
+        int expected = 9899456;
+        int actual = new Solution().minimumTimeRequired(jobs, k);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
